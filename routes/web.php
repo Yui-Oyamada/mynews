bb295676e08d89289b0eff3fe5d64e1baf6ba938
yui oyamada <yui.oyamada.2020@gmail.com>
2019-11-29 08:36:38 +0000

    commit

--- routes/web.php
+++ routes/web.php
@@ -17,28 +17,18 @@ Route::get('/', function () {
 
 Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
     Route::get('news/create', 'Admin\NewsController@add');
+    Route::post('news/create', 'Admin\NewsController@create');
+    Route::get('news/edit','Admin\NewsController@edit');
+    Route::post('news/edit','Admin\NewsController@update');
+    
+    Route::get('profile/create','Admin\ProfileController@add');
+    Route::post('profile/create', 'Admin\ProfileController@create');
+    Route::get('profile/edit','Admin\ProfileController@edit');
+    Route::post('profile/edit','Admin\ProfileController@update');
 });
-
-Route::get('admin/news/create','Admin\NewsController@add');
-
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::group(['prefix' => 'bbb'], function() {
-    Route::get('news/create', 'Admin\NewsController@add');
-});
-
-Route::get('admin/profile/create','Admin\ProfileController@add')->middleware('auth');
-
-Route::get('admin/profile/edit','Admin\ProfileController@edit')->middleware('auth');
 
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home')->middleware('auth');
