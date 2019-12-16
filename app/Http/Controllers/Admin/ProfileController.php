a383468a112290a974649899fc43ef5673643c3c
yui oyamada <yui.oyamada.2020@gmail.com>
2019-12-16 15:11:35 +0000

    commit

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -27,8 +27,7 @@ class ProfileController extends Controller
 
       // フォームから送信されてきた_tokenを削除する
       unset($form['_token']);
-      // フォームから送信されてきたimageを削除する
-      unset($form['image']);
+      
 
       // データベースに保存する
       $profile->fill($form);
@@ -36,16 +35,55 @@ class ProfileController extends Controller
       return redirect('admin/profile/create');
     }
     
-    public function edit()
+    public function index(Request $request)
+  {
+      $cond_name = $request->cond_title;
+      if ($cond_name!= '') {
+        
+          // 検索されたら検索結果を取得する
+          $posts = Profile::where('name', $cond_name)->get();
+      } else {
+          // それ以外はすべてのニュースを取得する
+          $posts = Profile::all();
+      }
+      return view('admin.profile.index', ['posts' => $posts, 'cond_name' => $cond_name]);
+  }
+    public function edit(Request $request)
     {
-      return view('admin.profile.edit');
+    
+       $profile = Profile::find($request->id);
+       if (empty($profile)) {
+            abort(404);    
+       }
+       return view('admin.profile.edit', ['profile_form' => $profile]);
+      
     }
     
-    public function update()
+    public function update(Request $request)
     {
-      return redirect('admin/profile/edit');
+      // Validationをかける
+      $this->validate($request, Profile::$rules);
+      // News Modelからデータを取得する
+      $profile = Profile::find($request->id);
+      // 送信されてきたフォームデータを格納する
+      $profile_form = $request->all();
+      
+      unset($profile_form['_token']);
+
+      // 該当するデータを上書きして保存する
+      $profile->fill($profile_form)->save();
+
+      return redirect('admin/profile');
     }
     
+    public function delete(Request $request)
+  {
+      // 該当するNews Modelを取得
+      $profile= Profile::find($request->id);
+      // 削除する
+      $profile->delete();
+      return redirect('admin/profile/');
+  } 
     
 }
 
